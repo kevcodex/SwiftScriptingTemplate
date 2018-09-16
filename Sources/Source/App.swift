f475a5234dbd188e5af97cb61e2749261c6f09db
Kevin Chen <kevchen2@gmail.com>
2018-09-16 16:50:00 -0700

    Put network layer as a dependency

--- Sources/Source/App.swift
+++ Sources/Source/App.swift
@@ -5,9 +5,11 @@
 //  Created by Kevin Chen on 6/26/18.
 
 import Foundation
+import MiniNe
 
 public class App {
     public static func start() {
         print("Hello World!")
+        let _ = MiniNeClient()
     }
 }
