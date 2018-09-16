755301f0160936ea08dfb6822176f670b05f82c8
Kevin Chen <kevchen2@gmail.com>
2018-09-16 13:24:23 -0700

    update network layer

--- Sources/Source/Networking/NetworkRequest.swift
+++ Sources/Source/Networking/NetworkRequest.swift
@@ -9,6 +9,9 @@ import Foundation
 
 public enum HTTPMethod: String {
     case get
+    case post
+    case put
+    case delete
     
     var name: String {
         return rawValue.uppercased()
@@ -21,6 +24,7 @@ public protocol NetworkRequest {
     var path: String { get }
     var method: HTTPMethod { get }
     var parameters: [String: Any]? { get }
+    var headers: [String: Any]? { get }
 }
 
 public extension NetworkRequest {
@@ -31,26 +35,54 @@ public extension NetworkRequest {
             return nil
         }
         
-        let url = baseURL.appendingPathComponent(path)
-        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        var urlRequest = URLRequest(url: baseURL)
         
-        switch method {
-        case .get:
-            let dictionary = parameters
-            let queryItems = dictionary?.map { key, value in
-                return URLQueryItem(name: key, value: String(describing: value))
-            }
-            components?.queryItems = queryItems
-        }
+        addPath(path, to: &urlRequest)
         
-        var urlRequest = URLRequest(url: url)
-        urlRequest.url = components?.url
-        urlRequest.httpMethod = method.name
+        addMethod(method, to: &urlRequest)
+        
+        addQueryParameters(parameters, to: &urlRequest)
+        
+        addHeaders(headers, to: &urlRequest)
         
         return urlRequest
     }
     
     func response(from data: Data, urlResponse: HTTPURLResponse) -> Response {
         return Response(statusCode: urlResponse.statusCode, data: data)
+    }
+}
+
+// MARK: - Private Helpers
+private extension NetworkRequest {
+    func addPath(_ path: String, to request: inout URLRequest) {
+        let url = request.url?.appendingPathComponent(path)
+        request.url = url
+    }
+    
+    func addMethod(_ method: HTTPMethod, to request: inout URLRequest) {
+        request.httpMethod = method.name
+    }
+    
+    func addQueryParameters(_ parameters: [String: Any]?, to request: inout URLRequest) {
+        guard let parameters = parameters,
+            let url = request.url else {
+            return
+        }
+        
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        
+        let queryItems = parameters.map { URLQueryItem(name: $0.key, value: String(describing: $0.value)) }
+        components?.queryItems = queryItems
+        
+        request.url = components?.url
+    }
+    
+    func addHeaders(_ headers: [String: Any]?, to request: inout URLRequest) {
+        guard let headers = headers else {
+            return
+        }
+        
+        headers.forEach { request.setValue(String(describing: $0.value), forHTTPHeaderField: $0.key) }
     }
 }
