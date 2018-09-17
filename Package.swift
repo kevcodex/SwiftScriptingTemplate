8892e7d2b38e4221af111924d95ef195b2f40f1f
Kevin Chen <kevchen2@gmail.com>
2018-09-16 17:00:47 -0700

    fix package

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     name: "ScriptingTemplate",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-//        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0"),
-        .package(url: "../MiniNe", .branch("master")),
+        //        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0"),
+        .package(url: "https://github.com/kevcodex/MiniNe", from: "1.0.0"),
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
